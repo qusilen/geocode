083465b658276487164c28456bedefdff79ee24e
Esra Özkutlu <esraozkutlu@hotmail.com>
2024-12-15 17:06:41 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ProjectInfo from './components/ProjectInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GeoCode</h1>
+      </header>
+      
+      <ProjectInfo />
+
+    </div>
+  );
+}
+
+export default App;
